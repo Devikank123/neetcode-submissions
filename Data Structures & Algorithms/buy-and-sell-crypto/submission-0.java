5d7da34011ce881fbc7fb92b5f0579b790b2569d
Devika N K <devikank343@gmail.com>
2026-05-20 10:26:19 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice=Integer.MAX_VALUE;
+        int maxProfit=Integer.MIN_VALUE;
+
+        for(int i=0;i<prices.length;i++){
+        int curr=prices[i];
+            minPrice=Math.min(minPrice,curr);
+            maxProfit=Math.max(maxProfit,curr-minPrice);
+        }
+        return maxProfit;
+    }
+}
